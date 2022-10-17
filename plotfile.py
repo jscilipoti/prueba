f87eabad01cb6f7adfe7f1999900b7b63a85a4f2
Jose Scilipoti <jscilipoti@Jose-IRQ>
2022-10-17 12:44:02 -0300

    plots

--- plotfile.py
+++ plotfile.py
@@ -8,10 +8,15 @@ class prueba1():
         prueba1_object = cls(nombre)
         return prueba1_object
 
+    @staticmethod
+    def usando_staticmethod():
+        print ("Hola")
+
 
 
 nombre = "jose"
-obj1 = prueba1(nombre)
+#obj1 = prueba1(nombre)
+obj1.usando_staticmethod
 obj2 = prueba1.usando_classmethod(nombre)
 print (obj1.name)
 print (obj2.name)
